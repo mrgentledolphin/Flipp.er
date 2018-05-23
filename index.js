1607e8640516c3c40490dc7a381730b97eca3ac5
Tommaso Iovane <tommaso.iovane@gmail.com>
2018-05-23 12:43:45 +0200

    mongoose 1

--- index.js
+++ index.js
@@ -2,10 +2,34 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const port = 3500
 /* MONGODB */
-const MongoClient = require('mongodb').MongoClient
-const assert = require('assert')
+const mongoose = require('mongoose')
 const url = 'mongodb://gd:gd@ds231460.mlab.com:31460/flipper'
+mongoose.connect(url)
+let db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
+
+let Schema = mongoose.Schema
+
+let usersSchema = new Schema({
+    first_name: String,
+    last_name: String,
+    age: Number,
+    address: String,
+    city: String,
+    email: String,
+    password: String,
+    posts: [String],
+    follows: [String],
+    favourite: [String],
+    profileImg: String,
+    backImg: String,
+    description: String
+})
+let postsScheme = new Schema({
+
+})
+let userModel = mongoose.model('users', usersSchema)
 
 
 console.log('Server started on port: ' + (port || 8080)) 
@@ -17,8 +41,31 @@ express()
     .set('view engine', 'hjs')
     .use(express.static(__dirname + './views'))
 /* GET AND POST REQUESTS */
-    .get('/', (req, res) => {
-        res.send('home page')
+
+    .get('/getAllUser', (req, res) => {
+        let query = userModel.find({})
+        query.select('first_name last_name')
+        query.sort()
+        query.exec((err, result) => {
+            res.send(result)
+        })
+        /* userModel.findOne({}, ('first_name last_name'), (err, users) => {
+            if (err) throw err
+            res.send(users)
+        }) */
+        /* userModel
+            .find()
+            .where('first_name').equals('Tommaso')
+            .where('age').gt(18).lt(50)
+            .limit(5)
+            .sort()
+            .select('first_name last_name')
+            .exec((err, data) => {
+                if (err) throw err
+                res.send(data)
+            }) 
+            findById()
+            */
     })
 
     .listen(port || 8080)
