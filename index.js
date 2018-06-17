08a31e7105104a372c9bff85081927c509ba616c
tommaso iovane <tommaso.iovane@gmail.com>
2018-06-17 20:19:53 +0200

    heroku£

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const session = require('express-session')
 const passport = require('passport')
 const bodyParser = require('body-parser')
-const port = 3500
+const port = 80
 const path = require('path')
 /* MONGODB */
 const mongoose = require('mongoose')
